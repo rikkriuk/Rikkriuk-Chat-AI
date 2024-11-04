26c4c6252c86c66f4a49d956233a2245cc9b8bc4
ikokywu <rikiridwan949@gmail.com>
2024-11-04 21:32:46 +0700

    Fix bug

--- server/src/index.js
+++ server/src/index.js
@@ -10,10 +10,11 @@ app.use(cors());
 
 app.post("/query", validateRequest, async (req, res) => {
   try {
+    console.log("Received request:", req.body);
     const response = await generateResponse(req.body.question);
     res.status(200).json({
       status: "success",
-      data: req.body,
+      data: response,
     });
   } catch (error) {
     res.status(500).json({
