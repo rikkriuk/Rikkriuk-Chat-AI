3f93497dcaf6e00ad6ad317a002df28317f6b64d
ikokywu <rikiridwan949@gmail.com>
2024-11-04 21:22:57 +0700

    Testing

--- server/src/index.js
+++ server/src/index.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.post("/query", validateRequest, async (req, res) => {
   try {
     console.log("Received request:", req.body);
-    const response = await generateResponse(req.body.question);
+    // const response = await generateResponse(req.body.question);
     res.status(200).json({
       status: "success",
-      data: response,
+      data: req.body,
     });
   } catch (error) {
     res.status(500).json({
